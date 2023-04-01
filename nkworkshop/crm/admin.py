d30b626a9727ad2d461610245bc37b9f50c652a2
EwenBALOUIN <balouin.e@free.fr>
2023-04-01 14:52:26 +0200

    ✨ add better admin

--- nkworkshop/crm/admin.py
+++ nkworkshop/crm/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from .models.customer import Customer
+from .models.action import Action
+from .models.company import Company
+
 
 admin.site.register(Customer)
+admin.site.register(Action)
+admin.site.register(Company)

8a93e4732ede6907ef16ba1daaa0e3e25922379e
EwenBALOUIN <balouin.e@free.fr>
2023-04-05 09:30:10 +0200

    ✨ create carts infos and donut chart

--- nkworkshop/crm/viewers/dashboard_view.py
+++ nkworkshop/crm/viewers/dashboard_view.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from crm.models import Customer
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    nb_contacts = 0
+    nb_client = 0
+    nb_lead = 0
+    nb_prospect = 0
+    conversion_rate = '0%'
+    nb_contacts = Customer.objects.all().count()
+    nb_client = Customer.objects.filter(status="client").count()
+    nb_lead = Customer.objects.filter(status="lead").count()
+    nb_prospect = Customer.objects.filter(status="prospect").count()
+    conversion_rate = str(round(nb_client / nb_contacts * 100, 2)) + '%'
+    return render(request, 'dashboard.html', {'nb_contacts': 0, 'nb_client': nb_client, 'nb_lead': nb_lead, 'nb_prospect': nb_prospect, 'conversion_rate': conversion_rate})
+
+
